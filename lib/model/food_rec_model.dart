5be43ba7d604affa12622837f1533ee7875ed3e9
SREERUP MOJUMDER <vhopel.sm@gmail.com>
2026-06-19 02:52:37 +0600

    Add Windows runner support with console output and DPI awareness
    - Introduced a manifest file for the Windows runner to enable DPI awareness and support for dark mode.
    - Implemented utility functions for creating a console and handling command line arguments in UTF-8.
    - Developed a Win32Window class to manage window creation, theming, and message handling, ensuring high DPI support.
    - Added necessary header files for utility functions and window management.

---
+++ lib/model/food_rec_model.dart
@@ -0,0 +1,21 @@
+class FoodRecModel {
+  final String title;
+  final String description;
+  final List<String> ingredients;
+
+  FoodRecModel({
+    required this.title,
+    required this.description,
+    required this.ingredients,
+  });
+
+
+
+  factory FoodRecModel.fromJson(Map<String, dynamic> json) {
+    return FoodRecModel(
+      title: json['title'] ?? '',
+      description: json['description'] ?? '',
+      ingredients: List<String>.from(json['ingredients'] ?? []),
+    );
+  }
+}
